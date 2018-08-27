480fdafe172a1ba0dc2987114e22b82503eabc4a
Brian Padilla <bpadil123@gmail.com>
2018-08-26 20:22:27 -0600

    need to fix eat me button

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green Chile Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom & Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+
